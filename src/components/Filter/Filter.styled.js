ad9665b656fd6322de0e66cdb8ef6377cc627c2c
YuriMykhalchenko <108886900+YuriMykhalchenko@users.noreply.github.com>
2023-03-26 12:01:26 +0200

    update

---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+import { theme } from '../../utils/theme';
+
+export const FilterWrapper = styled.label`
+  margin-bottom: ${theme.space.secondary}px;
+  font-size: ${theme.typography.text};
+  color: ${theme.colors.text};
+  font-style: italic;
+`;
+
+export const FilterInput = styled.input`
+  font-size: ${theme.typography.text};
+  padding: ${theme.space.secondary}px;
+  color: ${theme.colors.text};
+  border-radius: 5px;
+  margin-left: ${theme.space.secondary * 2}px;
+  font-style: italic;
+`;
